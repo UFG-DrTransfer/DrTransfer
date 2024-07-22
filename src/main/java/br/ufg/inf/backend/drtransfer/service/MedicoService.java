619a5290f1e1d6fbbefb1d4d50083ee5bea419a2
mathiastsq <mathias.matos@townsq.com.br>
2024-07-22 20:36:44 -0300

    MedicoService

--- src/main/java/br/ufg/inf/backend/drtransfer/service/MedicoService.java
+++ src/main/java/br/ufg/inf/backend/drtransfer/service/MedicoService.java
@@ -1,4 +1,28 @@
 package br.ufg.inf.backend.drtransfer.service;
 
-public class MedicoService {
+import br.ufg.inf.backend.drtransfer.model.Medico;
+import br.ufg.inf.backend.drtransfer.model.abstracts.SuperClass;
+import br.ufg.inf.backend.drtransfer.utils.GenericService;
+
+public class MedicoService extends GenericService<Medico, MedicoRepository> {
+
+
+    public MedicoService() {
+        super("Médico");
+    }
+
+    @Override
+    protected void atualizarEntidade(Medico entidadePersistida, Medico entidadeAtualizada) {
+        entidadePersistida.setNome(entidadeAtualizada.getNome());
+        entidadePersistida.setEmail(entidadeAtualizada.getEmail());
+        entidadePersistida.setTelefone(entidadeAtualizada.getTelefone());
+        entidadePersistida.setCpf(entidadeAtualizada.getCpf());
+        entidadePersistida.setSexo(entidadeAtualizada.getSexo());
+        entidadePersistida.setCrm(entidadeAtualizada.getCrm());
+        entidadePersistida.setAtivo(entidadeAtualizada.isAtivo());
+
+
+        entidadePersistida.setHospital(entidadeAtualizada.getHospital());
+
+    }
 }
