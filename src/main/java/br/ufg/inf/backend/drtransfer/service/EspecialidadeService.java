d29d36b5fd06a4c9373e228b7b3fa4e969a4ddae
mathiastsq <mathias.matos@townsq.com.br>
2024-07-22 22:26:47 -0300

    MedicoService implementado (90%) - terminar validações GenericService implementado.

--- src/main/java/br/ufg/inf/backend/drtransfer/service/EspecialidadeService.java
+++ src/main/java/br/ufg/inf/backend/drtransfer/service/EspecialidadeService.java
@@ -1,11 +1,21 @@
 package br.ufg.inf.backend.drtransfer.service;
 
+import br.ufg.inf.backend.drtransfer.model.Especialidade;
 import br.ufg.inf.backend.drtransfer.repository.EspecialidadeRepository;
+import br.ufg.inf.backend.drtransfer.utils.GenericService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EspecialidadeService {
-    @Autowired
-    private EspecialidadeRepository repository;
+public class EspecialidadeService extends GenericService<Especialidade, EspecialidadeRepository> {
+
+    public EspecialidadeService() {
+        super("Especialidade");
+    }
+
+    @Override
+    protected void atualizarEntidade(Especialidade entidadePersistida, Especialidade entidadeAtualizada) {
+        entidadePersistida.setNome(entidadeAtualizada.getNome());
+        entidadePersistida.setDescricao(entidadeAtualizada.getDescricao());
+    }
 }
--- src/main/java/br/ufg/inf/backend/drtransfer/service/MedicoService.java
+++ src/main/java/br/ufg/inf/backend/drtransfer/service/MedicoService.java
@@ -1,11 +1,17 @@
 package br.ufg.inf.backend.drtransfer.service;
 
+import br.ufg.inf.backend.drtransfer.exception.DrTransferException;
+import br.ufg.inf.backend.drtransfer.model.Especialidade;
+import br.ufg.inf.backend.drtransfer.model.Hospital;
 import br.ufg.inf.backend.drtransfer.model.Medico;
-import br.ufg.inf.backend.drtransfer.model.abstracts.SuperClass;
 import br.ufg.inf.backend.drtransfer.utils.GenericService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class MedicoService extends GenericService<Medico, MedicoRepository> {
 
+    @Autowired
+    private HospitalService hospitalService;
+    private EspecialidadeService especialidadeService;
 
     public MedicoService() {
         super("Médico");
@@ -13,16 +19,49 @@ public class MedicoService extends GenericService<Medico, MedicoRepository> {
 
     @Override
     protected void atualizarEntidade(Medico entidadePersistida, Medico entidadeAtualizada) {
-        entidadePersistida.setNome(entidadeAtualizada.getNome());
-        entidadePersistida.setEmail(entidadeAtualizada.getEmail());
+
+        if (validaString(entidadeAtualizada.getNome())) {
+            entidadePersistida.setNome(entidadeAtualizada.getNome());
+        }
+
+        if (validaString(entidadeAtualizada.getEmail())) {
+            entidadePersistida.setEmail(entidadeAtualizada.getEmail());
+        }
+
+        //fazer com as demais.
         entidadePersistida.setTelefone(entidadeAtualizada.getTelefone());
+
         entidadePersistida.setCpf(entidadeAtualizada.getCpf());
+
         entidadePersistida.setSexo(entidadeAtualizada.getSexo());
+
         entidadePersistida.setCrm(entidadeAtualizada.getCrm());
+
         entidadePersistida.setAtivo(entidadeAtualizada.isAtivo());
 
+        //Buscando o hospital que está vinculado com esse médico para validação.
+        Hospital hospitalValidado = hospitalService.findByEntidade(entidadeAtualizada.getHospital());
+        if (hospitalValidado != null) {
+            entidadePersistida.setHospital(hospitalValidado);
+        }
 
-        entidadePersistida.setHospital(entidadeAtualizada.getHospital());
-
+        //Buscando especialidade que está vinculada ao médico para validação.
+        Especialidade especialidadeValidada = especialidadeService.findByEntidade(entidadeAtualizada.getEspecialidade());
+        if (especialidadeValidada != null) {
+            especialidadeService.atualizarEntidade(especialidadeValidada, entidadeAtualizada.getEspecialidade());
+            entidadePersistida.setEspecialidade(especialidadeValidada);
+        }
     }
+
+    @Override
+    protected void validaEntidade(Medico entidade) {
+
+        if (!validaString(entidade.getNome())) {
+            throw new DrTransferException(" 'Nome' é obrigatório");
+        }
+        if (!validaString(entidade.getCrm())) {
+            throw new DrTransferException(" 'CRM' é obrigatório");
+        }
+    }
+
 }
