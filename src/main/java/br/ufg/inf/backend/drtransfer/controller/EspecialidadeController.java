58f56260a44f626450aff66bf9d60d6d047170fe
alexbeserra <alexbeserrab@gmail.com>
2024-07-24 23:23:04 -0300

    Acrescentado o extends GenericController na EspecialidadeController e acrescentado Optional na EspecialidadeRepository

--- src/main/java/br/ufg/inf/backend/drtransfer/controller/EspecialidadeController.java
+++ src/main/java/br/ufg/inf/backend/drtransfer/controller/EspecialidadeController.java
@@ -1,9 +1,14 @@
 package br.ufg.inf.backend.drtransfer.controller;
 
+import br.ufg.inf.backend.drtransfer.model.Cargo;
+import br.ufg.inf.backend.drtransfer.model.Especialidade;
+import br.ufg.inf.backend.drtransfer.service.CargoService;
+import br.ufg.inf.backend.drtransfer.service.EspecialidadeService;
+import br.ufg.inf.backend.drtransfer.utils.GenericController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/especialidade")
-public class EspecialidadeController {
+public class EspecialidadeController extends GenericController<Especialidade, EspecialidadeService> {
 }
