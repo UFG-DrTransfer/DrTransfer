bc1c0b080c9c59cefd2ec6c51d82f7c9ae636fc5
alexbeserra <alexbeserrab@gmail.com>
2024-07-25 23:53:10 -0300

    Alteração da especialidade corrigida

--- src/main/java/br/ufg/inf/backend/drtransfer/controller/EspecialidadeController.java
+++ src/main/java/br/ufg/inf/backend/drtransfer/controller/EspecialidadeController.java
@@ -1,8 +1,6 @@
 package br.ufg.inf.backend.drtransfer.controller;
 
-import br.ufg.inf.backend.drtransfer.model.Cargo;
 import br.ufg.inf.backend.drtransfer.model.Especialidade;
-import br.ufg.inf.backend.drtransfer.service.CargoService;
 import br.ufg.inf.backend.drtransfer.service.EspecialidadeService;
 import br.ufg.inf.backend.drtransfer.utils.GenericController;
 import org.springframework.web.bind.annotation.RequestMapping;
