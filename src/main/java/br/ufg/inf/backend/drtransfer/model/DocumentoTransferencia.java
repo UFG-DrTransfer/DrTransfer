8952c022bb9d67c50783ff2d149fb39008749dce
Danillo Tomaz Parreira <parreira16@gmail.com>
2024-07-30 19:49:21 -0300

    Update DocumentoTransferencia.java

--- src/main/java/br/ufg/inf/backend/drtransfer/model/DocumentoTransferencia.java
+++ src/main/java/br/ufg/inf/backend/drtransfer/model/DocumentoTransferencia.java
@@ -7,10 +7,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 @Entity
 @Data
