42fbfcf0aaff7a07cfd4ace42a31c903cffc5585
Danillo Tomaz Parreira <parreira16@gmail.com>
2024-07-30 23:04:20 -0300

    Att

--- src/main/java/br/ufg/inf/backend/drtransfer/DrtransferApplication.java
+++ src/main/java/br/ufg/inf/backend/drtransfer/DrtransferApplication.java
@@ -2,12 +2,13 @@ package br.ufg.inf.backend.drtransfer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"br.ufg.inf.backend.drtransfer", "br.ufg.inf.backend.openroute.controller"})
 public class DrtransferApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DrtransferApplication.class, args);
     }
-
 }
