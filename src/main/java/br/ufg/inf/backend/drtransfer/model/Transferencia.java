e399f37391d71e490aa74c476671ab059bbc4278
mathiastsq <mathias.matos@townsq.com.br>
2024-07-30 21:40:32 -0300

    TransferenciaService versao1

--- src/main/java/br/ufg/inf/backend/drtransfer/model/Transferencia.java
+++ src/main/java/br/ufg/inf/backend/drtransfer/model/Transferencia.java
@@ -3,9 +3,7 @@ package br.ufg.inf.backend.drtransfer.model;
 import br.ufg.inf.backend.drtransfer.enumeradores.MeioTransporte;
 import br.ufg.inf.backend.drtransfer.model.abstracts.SuperClass;
 import br.ufg.inf.backend.drtransfer.model.abstracts.SuperClassAtivo;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,21 +22,26 @@ public class Transferencia extends SuperClassAtivo {
     @Enumerated(EnumType.STRING)
     private MeioTransporte meioTransporte;
 
-
     private LocalDateTime horarioSaida;
     private LocalDateTime horarioPrevistoChegada;
 
     private Double distancia;
-//
-//    private Paciente paciente;
-//    private Medico medicoOrigem;
-//    private Hospital hospitalOrigem;
-//    private Medico medicoDestino;
-//    private Hospital hospitalDestino;
-//
-//    private MedicoRegulador medicoRegulador;
-//
-//    private DocumentoTransferencia documento;
-//    private Solicitacao solicitacao;
+    @OneToOne
+    private Solicitacao solicitacao;
+    @ManyToOne
+    private MedicoRegulador medicoRegulador;
+    @ManyToOne
+    private Medico medicoDestino;
+    @ManyToOne
+    private Hospital hospitalDestino;
+
+    @OneToOne
+    private Paciente paciente;
+    @OneToOne
+    private Medico medicoOrigem;
+    @OneToOne
+    private Hospital hospitalOrigem;
+    @OneToOne
+    private DocumentoTransferencia documento;
 
 }
--- src/main/java/br/ufg/inf/backend/drtransfer/service/TransferenciaService.java
+++ src/main/java/br/ufg/inf/backend/drtransfer/service/TransferenciaService.java
@@ -1,24 +1,30 @@
 package br.ufg.inf.backend.drtransfer.service;
 
-import br.ufg.inf.backend.drtransfer.enumeradores.MeioTransporte;
+
 import br.ufg.inf.backend.drtransfer.exception.DrTransferException;
 import br.ufg.inf.backend.drtransfer.model.Transferencia;
 import br.ufg.inf.backend.drtransfer.repository.TransferenciaRepository;
 import br.ufg.inf.backend.drtransfer.utils.GenericService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 @Service
 public class TransferenciaService extends GenericService<Transferencia, TransferenciaRepository> {
-    public TransferenciaService() {
+
+    @Autowired
+    private SolicitacaoService solicitacaoService;
+    @Autowired
+    private MedicoService medicoService;
+    @Autowired
+    private MedicoReguladorService medicoReguladorService;
+
+
+    public TransferenciaService(MedicoReguladorService medicoReguladorService) {
         super("Transferencia");
+        this.medicoReguladorService = medicoReguladorService;
     }
 
-
-//    private MeioTransporte meioTransporte;
-//    private LocalDateTime horarioSaida;
-//    private LocalDateTime horarioPrevistoChegada;
-//    private Double distancia;
 
     @Override
     protected void padronizaCampos(Transferencia entidade) throws DrTransferException {
@@ -27,16 +33,60 @@ public class TransferenciaService extends GenericService<Transferencia, Transfer
 
     @Override
     protected void validaEntidade(Transferencia entidade) throws DrTransferException {
+        campoObrigatorio(entidade.getMeioTransporte(), "Meio de Transporte");
+        campoObrigatorio(entidade.getHorarioSaida(), "Horário de saída");
+        campoObrigatorio(entidade.getSolicitacao(), "Solicitacao");
+        campoObrigatorio(entidade.getMedicoRegulador(), "Medico Regulador");
+        campoObrigatorio(entidade.getMedicoDestino(), "Médico destino");
 
     }
 
     @Override
+
     protected void atualizaVinculos(Transferencia entidade) throws DrTransferException {
+        if (entidade.getSolicitacao() != null) {
+            if (entidade.getSolicitacao().isNovo()) {
+                throw new DrTransferException(HttpStatus.BAD_REQUEST, ID_INVALIDO, "Solicitação");
+            } else {
+                entidade.setSolicitacao(solicitacaoService.findByEntidade(entidade.getSolicitacao()));
+                entidade.setPaciente(entidade.getSolicitacao().getPaciente());
+                entidade.setMedicoOrigem(entidade.getSolicitacao().getMedico());
+                entidade.setHospitalOrigem(entidade.getSolicitacao().getMedico().getHospital());
+                entidade.setDocumento(entidade.getSolicitacao().getDocumento());
+            }
+        }
+
+        if (entidade.getMedicoRegulador() != null) {
+            if (entidade.getMedicoRegulador().isNovo()) {
+                throw new DrTransferException(HttpStatus.BAD_REQUEST, ID_INVALIDO, "Medico Regulador");
+            } else {
+                entidade.setMedicoRegulador(medicoReguladorService.findByEntidade(entidade.getMedicoRegulador()));
+            }
+        }
+
+        if (entidade.getMedicoDestino() != null) {
+            if (entidade.getMedicoDestino().isNovo()) {
+                throw new DrTransferException(HttpStatus.BAD_REQUEST, ID_INVALIDO, "Medico Destino");
+            } else {
+                entidade.setMedicoDestino(medicoService.findByEntidade(entidade.getMedicoDestino()));
+                entidade.setHospitalDestino(entidade.getMedicoDestino().getHospital());
+
+            }
+        }
+
 
     }
 
     @Override
     protected void atualizarEntidade(Transferencia entidadePersistida, Transferencia entidadeAtualizada) throws DrTransferException {
+        atualizaCampo(entidadePersistida, entidadeAtualizada, "meioTransporte");
         atualizaCampo(entidadePersistida, entidadeAtualizada, "horarioSaida");
+//        atualizaCampo(entidadePersistida, entidadeAtualizada, "horarioPrevistoChegada");
+//        atualizaCampo(entidadePersistida, entidadeAtualizada, "distancia");
+        atualizaCampo(entidadePersistida, entidadeAtualizada, "medicoRegulador");
+        atualizaCampo(entidadePersistida, entidadeAtualizada, "medicoDestino");
+        atualizaCampo(entidadePersistida, entidadeAtualizada, "hospitalDestino");
+        atualizaCampo(entidadePersistida, entidadeAtualizada, "solicitacao");
+
     }
 }
